e85e84f892cc61d75d1ac7c66a3ac7a27c6d1299
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2024-11-05 09:10:43 -0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import './App.css'
 import Header from './component/header/Header'
 import Nav from './component/nav/Nav'
 import About from './component/about/About'
-import Experience from './component/experience/Experience'
-import Services from './component/services/Services'
-import Portfolio from './component/portfolio/Portfolio'
-import Testimonial from './component/testimonial/Testimonial'
+// import Experience from './component/experience/Experience'
+// import Services from './component/services/Services'
+// import Portfolio from './component/portfolio/Portfolio'
+// import Testimonial from './component/testimonial/Testimonial'
 import Contact from './component/contact/Contact'
-import Footer from './component/footer/Footer'
+// import Footer from './component/footer/Footer'
 
 
 function App() {
@@ -20,12 +20,12 @@ function App() {
       <Header/>
       <Nav/>
       <About/>
-    <Experience/>
+    {/* <Experience/>
     <Services/>
     <Portfolio/>
-    <Testimonial/>
+    <Testimonial/> */}
     <Contact/>
-    <Footer/>
+    {/* <Footer/> */}
 
     
      
--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,10 +1,65 @@
 import './Contact.css'
-
+import { MdEmail } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa6";
 import React from 'react'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_07nh3dd', 'template_xr3634e', form.current, {
+        publicKey: 'LW---iXM8TMcefiQl',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+      e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className="contact__options">
+    <article className="contact_option">
+    <MdEmail className='contact__option-icon' />
+      <h4>Email</h4>
+      <h5>samuelokolie2012@gmail.com</h5>
+      <a href="mailto:dummysamuelokolie2012@gmail.com" target='_blank'> Send a message</a>
+    </article>
+
+    
+
+    <article className="contact_option">
+    <FaWhatsapp className='contact__option-icon'/>
+      <h4>Whatsapp</h4>
+      <h5>+2348072676311</h5>
+      <a href="https://api.whatspp.com/send?=+2348072676311" target='_blank'> Send a message</a>
+    </article>
+        </div>
+        {/* end of contact options */}
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name here' required />
+          <input type="email" name='nemail' placeholder='Your Email here' required />
+          <textarea name="message" rows='7' placeholder='Your message' id="" required></textarea>
+          <button type='submit' className='btn btn-primary'> Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
